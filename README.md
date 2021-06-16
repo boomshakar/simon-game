2e221b152aa96ac2ccab12956078d9bf43d0650f
Oluwafemi Ṣosanya <Hefehmzy@gmail.com>
2021-06-16 15:13:06 +0100

    commit

---
+++ README.md
@@ -0,0 +1,47 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+This is a copy cat of the popular simon game which is used to test how long/well we store patterns in our memory.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- Press any key to START the game. The Computer will give the first signal. You the user will Repeat the signal by pressing the same color lens.
+
+- The Computer will duplicate the first signal and add one. Repeat these two signals by pressing the same color lenses, in order.
+
+- The Computer will duplicate these first two signals and add one.
+
+- Continue playing as long as you can repeat each sequence of signals correctly.
+
+- If you fail to repeat a sequence exactly, The Computer responds with a "RAZZ" sound and a Red flash. This means you've lost, and the game is over but you can also press any key again to start over.
+
+### Screenshot
+
+![](./simon.png)
+
+### Links
+
+- Solution URL: [Solution URL here](https://github.com/boomshakar/roll-your-dice)
+- Live Site URL: [Live site URL here](https://rollyourdice.netlify.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- JavaScript
+- CSS
+
+## Author
+
+- Website - [Oluwafemi Sosanya](https://boomshakar.netlify.app/contact.html)
+- Twitter - [@_boomshakar_](https://www.twitter.com/_boomshakar)
---
+++ game.js
@@ -0,0 +1,75 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).on("keypress", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  var newCurrentColor = "#" + currentColour;
+  $(newCurrentColor).addClass("pressed");
+  setTimeout(function () {
+    $(newCurrentColor).removeClass("pressed");
+  }, 100);
+}
